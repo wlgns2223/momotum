54b42654e79752e92fd392a8cde78f106dbc32e8
jihoon <wlgns2223@gmail.com>
2021-05-24 23:57:00 +0900

    clockmanager upgrade

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,29 @@
-const getTime = () => {
-    const clockContainer = document.querySelector('.js-clock');
-    const clockTitle = clockContainer.querySelector('.js-title');
-    const date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+class ClockManager {
 
-    hours = `${hours < 10 ? `0${hours}` : hours}`;
-    minutes = `${minutes < 10 ? `0${minutes}` : minutes}`;
-    seconds = `${seconds < 10 ? `0${seconds}` : seconds}`;
-   
+    constructor() {
+        this.init();
+    }
 
-    clockTitle.textContent = `${hours}:${minutes}:${seconds}`
+    init() {
+
+        this.renderTime();
+        setInterval(this.renderTime,1000);
+
+    }
+
+    renderTime() {
+        const clockContainer = document.querySelector('.js-clock');
+        const clockTitle = document.querySelector('.js-title');
+        const date = new Date();
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+
+        hours = `${hours < 10 ? `0${hours}` : hours}`;
+        minutes = `${minutes < 10 ? `0${minutes}` : minutes}`;
+        seconds = `${seconds < 10 ? `0${seconds}` : seconds}`;
+
+        clockTitle.textContent = `${hours}:${minutes}:${seconds}`;
+    }
 }
-
-function init() {
-    getTime();
-    setInterval(getTime,1000);
-}
-
-init();
