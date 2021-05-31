750b36a466c06609a60ebac687ae3416e58b9ece
jihoon <wlgns2223@gmail.com>
2021-05-31 20:52:04 +0900

    Separate background manager and background App

--- js/bg.js
+++ js/bg.js
@@ -3,26 +3,26 @@ class BackgroundManager {
     static get IMG_NUM() {
         return 5;
     }
-
-    constructor() {
-        
-        this.init();
-    }
     
-    init() {
-        const numberOfImages = this.getRandomNumber();
-        this.renderBackground(numberOfImages);
-    }
-
-    renderBackground(imgNum) {
-        const body = document.querySelector('body');
-        const img = new Image();
-        img.src = `./images/${imgNum}.jpg`;
-        img.classList.add('bgImage');
-        body.appendChild(img);
-    }
+    constructor() { }
 
     getRandomNumber() {
         return Math.floor(Math.random() * BackgroundManager.IMG_NUM) + 1;
+    }
+}
+
+class BackgroundApp {
+    constructor() {
+        this.backgroundManager = new BackgroundManager();
+        this.renderBackground();
+    }
+
+    renderBackground(){
+        const randomImageNumber = this.backgroundManager.getRandomNumber();
+        const body = document.querySelector('body');
+        const img = new Image();
+        img.src = `./images/${randomImageNumber}.jpg`;
+        img.classList.add('bgImage');
+        body.appendChild(img);
     }
 }
