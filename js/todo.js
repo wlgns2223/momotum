2bdbd5a66b1c790e8b2533d2e589db84145a04e6
jihoon <wlgns2223@gmail.com>
2021-05-31 13:46:29 +0900

    todoapp created

--- js/todo.js
+++ js/todo.js
@@ -1,74 +1,89 @@
-const toDoForm = document.querySelector('.js-toDoForm');
-const toDoInput = toDoForm.querySelector('input');
-const toDoList = document.querySelector('.js-toDoList');
-const TODOS_LS = 'toDos';
-let todos = [];
+class ToDo {
+    constructor(content, done= false){
+        this.content = content;
+        this.done = done;
+    }
 
+    toggle() {
+        this.done = ! this.done;
+    }
+}
 
-const loadToDo = () =>{
-    const loadedTodo = localStorage.getItem(TODOS_LS);
-    toDoForm.addEventListener('submit',toDohandleSubmit);
-    if(loadedTodo !== null){
-        const parsedToDos = JSON.parse(loadedTodo);
-        parsedToDos.forEach((todos) => {
-            paintToDo(todos.text);
+class ToDoManager {
+
+    constructor(todos = [] ) {
+        this._todos = [];
+        this._todos.forEach(todo => {
+            this.addTodo(todo.content, todo.done);
         });
-    } 
+    }
+
+    addTodo(content, done = false){
+        const newTodo = new ToDo(content,done);
+        this._todos.push(newTodo);
+        return newTodo;
+    }
+
+    getList() {
+        return this._todos;
+    }
+
+    get leftCount() {
+        return this._todos.reduce((prev,cur) => {
+            if(cur.done === false) {
+                return prev++;
+            } else {
+                return prev;
+            }
+        });
+    }
 }
 
-const paintToDo = (text) =>{
-    const li = document.createElement('li');
+class TodoApp {
+    constructor(todos) {
+        this.todoManager = new ToDoManager(todos);
+        this.todoList = document.querySelector('.todo .todo-list');
+        this.addBtn = document.querySelector('.todo .todo-add .add-button');
+        this.bindEvents();
+        this.renderTodo();
+    }
 
-    const delBtn = document.createElement('button');
-    delBtn.textContent = "😀";
-    delBtn.addEventListener('click',deletToDo);
+    renderTodo() {
+        this.todoList.innerHTML = '';
+        this.todoManager.getList().forEach((todo, i) => {
+            const todoElement = this.createTodoElements(todo,i);
+            this.todoList.appendChild(todoElement);
+        });
+    }
 
-    const span = document.createElement('span');
-    span.textContent = text;
+    createTodoElements(todo, i) {
+        const li = document.createElement('li');
+        li.id = "todo-"+i;
+        li.className = "todoItem";
+        li.innerHTML = 
+            `<input type ="checkbox" ${todo.done ? "checked ": ""}>
+             <label>${todo.content}</label>`;
 
-    const newID = todos.length + 1;
+        return li;
+    }
 
-    li.appendChild(delBtn);
-    li.appendChild(span)
-    li.id = newID;
+    bindEvents() {
+        this.addBtn.addEventListener('click',event => {
+            event.preventDefault();
+            let text = document.querySelector(`.todo-add input[type="text"]`);
+            this.todoManager.addTodo(text.value);
+            text.value='';
+            this.renderTodo();
+        });
 
-    toDoList.appendChild(li);
-    const todoObj = {
-        text : text,
-        id: newID
-    };
-    todos.push(todoObj);
-    saveToDos();
+        this.todoList.addEventListener('click',event => {
+            if(event.target.nodeName === 'INPUT' 
+               && event.target.parentElement.className === 'todoItem'){
+                   const clicked = event.target.parentElement;
+                   const index = clicked.id.replace('todo-','');
+                   this.todoManager.getList()[index].toggle();
+                   this.renderTodo();
+               }
+        });
+    }
 }
-
-const deletToDo = (event) => {
-    const btn = event.target;
-    const li = btn.parentElement;
-    toDoList.removeChild(li);
-
-    const cleanToDos = todos.filter((item) => item.id !== parseInt(li.id));
-    todos = cleanToDos;
-    saveToDos();
-
-}
-
-const saveToDos = () => {
-    localStorage.setItem(TODOS_LS,JSON.stringify(todos));
-}
-
-const toDohandleSubmit = (event) =>{
-    event.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-    toDoInput.value = "";
-}
-
-
-
-
-
-function init() {
-    loadToDo();
-}
-
-init();
